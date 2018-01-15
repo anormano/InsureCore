73c87251e2e7ac18dd11999c8f5e0e4087be357b
norman <gue@normansyah.com>
2018-01-15 07:20:29 +0700

    ok

--- InsureCore.Module/BusinessObjects/Administration/User.cs
+++ InsureCore.Module/BusinessObjects/Administration/User.cs
@@ -21,7 +21,6 @@ namespace InsureCore.Module.BusinessObjects.Administration
     [CurrentUserDisplayImage("Photo")]
     [CreatableItem(false)]
     //[ImageName("BO_Contact")]
-    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
     [DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
     //[Persistent("DatabaseTableName")]
     // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
